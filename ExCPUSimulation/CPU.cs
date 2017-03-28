495d30c4d6b49cd95f6e385677378279c1739423
John H <power_mand@hotmail.com>
2017-03-28 10:06:31 +0200

    Ex3

--- ExCPUSimulation/CPU.cs
+++ ExCPUSimulation/CPU.cs
@@ -29,24 +29,36 @@ namespace ExCPUSimulation
 				int value = int.Parse(Ram.Addresses[address].Substring(2, 4));
 				switch (opcode)
 				{
-					case 1:
+					case 1: // Load
 						Accumulator = value;
 						Console.WriteLine("Load value " + value +" from address " + address + " into accumulator.");
 						break;
-					case 2:
+					case 2: // Add
 						Accumulator += value;
 						Console.WriteLine("Add value " + value +" from address " + address + " and store in accumulator");
 						break;
-					case 3:
+					case 3: // Subtract
 						if (Accumulator - value < 0) throw new Exception("The value subtracted from accumulator is greater than the value in the accumulator");
 						Accumulator -= value;
 						Console.WriteLine("Subtract value " + value + " from address " + address + " and store in accumulator");
 						break;
-					case 6:
+					case 6: // Store
 						Ram.Addresses[address] = Accumulator.ToString().PadLeft(6, '0');
 						Console.WriteLine("Store value " + Accumulator + " to address " + address);
 						break;
-					case 99:
+					case 7:
+						ProgramCounter = address-1;
+						Console.WriteLine("Jump to address " + address);
+						break;
+					case 9: // Test greater than
+						if (value > Accumulator)
+						{
+							ProgramCounter++;
+							Console.WriteLine("Value is greater than accumulator");
+						}
+						break;
+					case 99: // Stop
+						Console.WriteLine("STOP");
 						running = false;
 						break;
 				}
--- ExCPUSimulation/Program.cs
+++ ExCPUSimulation/Program.cs
@@ -11,18 +11,26 @@ namespace ExCPUSimulation
 		static void Main(string[] args)
 		{
 			Ram ram = new Ram(40);
-			ram.Addresses[0] = "010020";
-			ram.Addresses[1] = "020021";
-			ram.Addresses[2] = "060022";
-			ram.Addresses[3] = "990000";
+			ram.Addresses[0] = "010020"; // Load 100
+			ram.Addresses[1] = "030021"; // Subtract 70, accumulator = 30
+			ram.Addresses[2] = "090010"; // Test if 520 is greater than accumulator
+			ram.Addresses[3] = "070006"; // If not greater jump to 6
+			ram.Addresses[4] = "070008"; // If greater, jump to 8
+			ram.Addresses[6] = "060031"; // Store to 31
+			ram.Addresses[7] = "070009"; // Jump to end
+			ram.Addresses[8] = "060030"; // Store to 30
+			ram.Addresses[9] = "990000"; // Stop
+
+			// Values
+			ram.Addresses[10] = "000520";
 			ram.Addresses[20] = "000100";
-			ram.Addresses[21] = "000123";
+			ram.Addresses[21] = "000070";
 
 			CPU cpu = new CPU(ram);
 
 			cpu.Execute();
 			Console.WriteLine();
-			ram.PrintAll();
+			//ram.PrintAll();
 			Console.ReadKey();
 		}
 		
